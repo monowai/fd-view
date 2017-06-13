e350f8c23f98efbeea65463658fa8970eecb1fea
dmty <dmtyak@gmail.com>
2017-06-13 23:05:48 +1200

    Search results scroll

--- src/app/search/fd-search-results.react.js
+++ src/app/search/fd-search-results.react.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-import {Row, Col, Button} from 'react-bootstrap';
+import {Row, Col} from 'react-bootstrap';
+import ResultsScroller from './results-scroller.react';
+import {getAngularService} from '../services/angular-react-helper';
 
-import SearchResult from './search-result.react';
-
-import {selectProvider, selectTypes, fetchTypes} from '../config/actions';
-
-const FdSearchResults = ({entities, total, index, setFilter}) => {
-  if (entities.length) {
+const FdSearchResults = ({total}) => {
+  if (total > 0) {
+    const store = getAngularService(document, '$ngRedux');
     return (
       <Row id="results">
         <Col md={12}>
           <div className="box-body">
-            <ul className="timeline" infinite-scroll="$ctrl.sr.nextPage()" infinite-scroll-distance="1">
-              <li className="time-label">
-                <span className="bg-green">{total} Search Results</span>
-              </li>
-
-              {entities.map((searchResult, i) => (<SearchResult key={i} entity={searchResult} setFilter={setFilter} />))}
-
-              {(index < total &&
-                  <li className="text-center">
-                    <Button bsStyle="default" bsSize="small" href="$ctrl.sr.nextPage()">
-                      <i className="fa fa-arrow-down text-muted" />
-                      LOAD MORE
-                    </Button>
-                  </li>)}
-            </ul>
+            <ResultsScroller store={store}/>
           </div>
         </Col>
       </Row>
@@ -37,20 +22,7 @@ const FdSearchResults = ({entities, total, index, setFilter}) => {
 };
 
 const mapStateToProps = state => ({
-  entities: state.reducer.search.entities,
-  total: state.reducer.search.total,
-  index: state.reducer.search.index
+  total: state.reducer.search.total
 });
 
-const mapDispatchToProps = dispatch => ({
-  setFilter(fortress, docType) {
-    dispatch(selectProvider(fortress));
-    if (docType) {
-      dispatch(selectTypes([{name: docType}]));
-    } else {
-      dispatch(fetchTypes(fortress));
-    }
-  }
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(FdSearchResults);
+export default connect(mapStateToProps)(FdSearchResults);
---
+++ src/app/search/results-scroller.react.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import InfiniteScroll from 'redux-infinite-scroll';
+
+import {Button} from 'react-bootstrap';
+import SearchResult from './search-result.react';
+import {selectProvider, selectTypes, fetchTypes} from '../config/actions';
+import {runTermSearch} from './actions';
+
+const ResultsScroller = ({entities, total, index, onSearch, setFilter, ...params}) => {
+  const loadMore = () => {
+    onSearch({index, ...params});
+  };
+
+  return (
+    <InfiniteScroll
+      loadMore={loadMore} hasMore={index < total} elementIsScrollable={false} className="timeline"
+    >
+      <li className="time-label">
+        <span className="bg-green">{total} Search Results</span>
+      </li>
+      {entities.map((searchResult, i) => (<SearchResult key={i} entity={searchResult} setFilter={setFilter} />))}
+      {(index < total &&
+        <li className="text-center">
+          <Button bsStyle="default" bsSize="small" onClick={loadMore}>
+            <i className="fa fa-arrow-down text-muted" />
+            LOAD MORE
+          </Button>
+        </li>
+      )}
+    </InfiniteScroll>
+  );
+};
+
+const mapStateToProps = state => ({
+  entities: state.reducer.search.entities,
+  total: state.reducer.search.total,
+  // search configs from the state:
+  term: state.reducer.search.term,
+  fortress: state.reducer.provider.fortress,
+  types: state.reducer.provider.typesSelected,
+  index: state.reducer.search.index,
+  filter: state.reducer.search.filter
+});
+
+const mapDispatchToProps = dispatch => ({
+  setFilter(fortress, docType) {
+    dispatch(selectProvider(fortress));
+    if (docType) {
+      dispatch(selectTypes([{name: docType}]));
+    } else {
+      dispatch(fetchTypes(fortress));
+    }
+  },
+  onSearch(params) {
+    const {term, fortress, types, index, filter} = params;
+    dispatch(runTermSearch({
+      searchText: term || '*',
+      fortress: fortress.length ? fortress[0].name : null,
+      types: types.map(t => t.name),
+      from: index,
+      filter: Object.keys(filter).length ? filter : null
+    }));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ResultsScroller);
